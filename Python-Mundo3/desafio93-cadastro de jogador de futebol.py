744b282f422f60457471c4697adf6223f2e5ef95
ge39 <ge39.medeiros@gmail.com>
2020-04-29 08:47:35 -0300

    desafio93-cadastro de jogador de futebol

--- Python-Mundo3/desafio93-cadastro de jogador de futebol.py
+++ Python-Mundo3/desafio93-cadastro de jogador de futebol.py
@@ -4,10 +4,15 @@ O programa vai ler o nome do jogador e quantas partidas ele jogou.
 Depois vai ler a quantidade de gols feitos em cada partida.
 No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato
 '''
-player = dict()
+player = []
+gols = {}
 
-while True:
-    player['nome'] = str(input('Nome Jogador: ').upper())
-    player['partida'] = int(input(f'Nº de Partidas {player["nome"]}: '))
-    for p in range(player['partida']):
-        gols = int(input(f'Quantos gols na {p + 1}ª partida: '))
+player.append(str(input('Nome Jogador: ').upper()))
+player.append(int(input(f'Nº de Partidas {player[0]}: ')))
+for p in range(player[1]):
+    gols['gol'] = int(input(f'Quantos gols na {p + 1}ª partida: '))
+    player.append(gols.copy())
+for e in player:
+
+      print(f'=> {e}', end=" ")
+
