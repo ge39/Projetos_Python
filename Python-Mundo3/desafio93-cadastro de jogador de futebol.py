91a8654cf2bbee5be1d117f972f04bfc4f89325d
ge39 <ge39.medeiros@gmail.com>
2020-05-04 08:32:01 -0300

    desafio93-cadastro de jogador de futebol

--- Python-Mundo3/desafio93-cadastro de jogador de futebol.py
+++ Python-Mundo3/desafio93-cadastro de jogador de futebol.py
@@ -6,11 +6,23 @@ No final, tudo isso será guardado em um dicionário, incluindo o total de gols 
 '''
 jogador = dict()
 partidas = list()
-
 jogador['nome'] = str(input('Nome Jogador: ').upper())
 tot = int(input(f'Nº de Partidas {jogador["nome"]}: '))
 for p in range(0, tot):
-   partidas.append = int(input(f'Quantos gols na {p + 1}ª partida: '))
+   partidas.append(int(input(f'Quantos gols na {p + 1}ª partida: ')))
 jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-='*30)
 print(jogador)
-print(partidas)
+#print(partidas)
+print('-='*30)
+for k, v in jogador.items():
+   print(f' O campo {k} tem o valor {v}')
+print('-='*30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
+print('-='*30)
+for i, v in enumerate(jogador['gols']):
+   print(f'    => Na partida {i+1}, fez {v} gols')
+print(f'Foi um total de {jogador["total"]} gols.')
+
+
