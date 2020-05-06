668aa0b071681d7f588f62a7d4d35a056f0a094f
ge39 <ge39.medeiros@gmail.com>
2020-05-06 08:43:23 -0300

    desafio94-unindo dicionarios e listas

--- Python-Mundo3/desafio94-unindo dicionarios e listas.py
+++ Python-Mundo3/desafio94-unindo dicionarios e listas.py
@@ -1,5 +1,6 @@
 '''
-Crie um programa que leia nome,sexo e idade de várias pessoas, guardadndo os dados de cada pessoa em um dicionario.
+Crie um programa que leia nome,sexo e idade de várias pessoas, guardadndo os dados de cada pessoa
+em um dicionario.
 e todos os dicionarios em uma lista.
 No final, mostre:
 A - Quantas pessoas foram cadastradas
@@ -7,3 +8,25 @@ B - A média de idade do grupo
 C - Uma lista com todas as mulheres
 D - Uma lista com todas as pessoas com idade acima da média.
 '''
+pessoa = dict()
+galera = list()
+
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input("Nome :").upper())
+    while True:
+        pessoa['sexo'] = str(input("Sexo [M/F]: ").upper())[0]
+        if pessoa['sexo'] in 'FM':
+            break
+        print(f'ERROR!,por favor, digite apenas M ou F')
+    pessoa['idade'] = int(input('idade: '))
+    while True:
+        resp = str(input('Deseja continuar? [S/N]: ').upper())
+        if resp in 'SN':
+             break
+        print("ERROR! Responda S ou N.")
+    if resp in 'SN':
+        break
+print('=-'*30)
+print(pessoa)
+
