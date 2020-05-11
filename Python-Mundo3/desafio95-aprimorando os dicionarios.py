f8e328eba83209840f4ae5814054818ad1ace329
ge39 <ge39.medeiros@gmail.com>
2020-05-11 08:17:50 -0300

    desafio95-aprimorando os dicionarios

--- Python-Mundo3/desafio95-aprimorando os dicionarios.py
+++ Python-Mundo3/desafio95-aprimorando os dicionarios.py
@@ -1,4 +1,15 @@
 '''
+PARTE 1
+Crie um programa que leia nome,sexo e idade de várias pessoas, guardadndo os dados de cada pessoa
+em um dicionario.
+e todos os dicionarios em uma lista.
+No final, mostre:
+A - Quantas pessoas foram cadastradas
+B - A média de idade do grupo
+C - Uma lista com todas as mulheres
+D - Uma lista com todas as pessoas com idade acima da média.
+
+PARTE 2
 Aprimore o desafio 93, para que ele funcione com varios jogadores,
 incluindo um sistema de visualização de detalhes do aproveitamento
 de cada jogador
