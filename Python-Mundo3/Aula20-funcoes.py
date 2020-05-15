9731fe307429eb629f49f1e6bdd3d287467cac0a
ge39 <ge39.medeiros@gmail.com>
2020-05-15 08:38:49 -0300

    Aula20-funcoes

---
+++ Python-Mundo3/Aula20-funcoes.py
@@ -0,0 +1,11 @@
+'''Funçoes python, cria comando personalizados e parametros'''
+
+def titulo(txt):
+    print('-' * 30)
+    print(txt)
+    print('-'* 30)
+#  programa principal
+titulo('CURSO EM VIDEO')
+titulo('APRENDA PYTHON')
+titulo('GUSTAVO GUANABARA')
+titulo('Aluno - Jeremias')
