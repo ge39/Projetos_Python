19e75143172ef2456fc1fb642ccc5e08b7e4ef66
ge39 <ge39.medeiros@gmail.com>
2020-05-21 08:37:00 -0300

    desafio96-funcao que calcula area

---
+++ Python-Mundo3/desafio96-funcao que calcula area.py
@@ -0,0 +1,8 @@
+'''
+    Faca u,m programa que tenha uma funcao chamada area ().
+    que receba as dimensoes de um terreno retangular(largura e comprimento) e
+    mostre a área do terreno
+'''
+
+
+print('CONTROLE DE TERRENO')
