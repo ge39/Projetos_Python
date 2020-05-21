ff7757a590677a9be8f8544687892632b5c00e21
ge39 <ge39.medeiros@gmail.com>
2020-05-21 08:01:11 -0300

    Aula20-funcoes - parte1

--- Python-Mundo3/Aula20-funcoes.py
+++ Python-Mundo3/Aula20-funcoes.py
@@ -1,11 +1,54 @@
 '''Funçoes python, cria comando personalizados e parametros'''
 
-def titulo(txt):
+#  funcoes ou rotinas
+def linha():
+    print('linha - ' * 10)
+
+linha()
+
+
+
+#  funcoes com parametros , argumentos
+def titulo(msg):
     print('-' * 30)
-    print(txt)
+    print(msg)
     print('-'* 30)
+
+
 #  programa principal
 titulo('CURSO EM VIDEO')
 titulo('APRENDA PYTHON')
 titulo('GUSTAVO GUANABARA')
 titulo('Aluno - Jeremias')
+
+print('criando uma funçao soma')
+def soma(a, b):
+    S = a + b
+    print(S)
+
+
+soma(4, 5)
+soma(2, 5)
+soma(2, 15)
+soma(2, 8)
+print('-' * 30)
+print('Como empacotar parametros, entao, ')
+# o simbolo * define desempacotar parametros, entao, ele entenderá que voce passará varios parametros, e
+# nao sabe quantos sao, entao voce vai pegar todos os parametros e jogar dentro de num, isso significa,
+# se voce jogar um ou 10 mil parametros, ele vai jogar dentro de num.
+
+def contador(*num):
+    tam = len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam} numeros')
+
+    for valor in num:
+        print(f' {valor}', end='')
+    print(' - FIM!')
+
+
+contador(2, 1, 9)
+contador(7, 0, 3, 8,)
+contador(1)
+
+
+
