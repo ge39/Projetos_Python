0d48675c84ea25b1e2afb901ff812bb55a599574
ge39 <ge39.medeiros@gmail.com>
2020-05-21 20:57:58 -0300

    desafio97-um print especial

--- Python-Mundo3/desafio97-um print especial.py
+++ Python-Mundo3/desafio97-um print especial.py
@@ -2,3 +2,12 @@
     faça um programa que tenha a função chamada escreva()
     que receba um texto qualquer como parametro e mostre uma mensagem com tamanho adaptavel
 '''
+
+def escreva(txt,l):
+    print(txt)
+    print('-' * l)
+
+
+texto = str(input('texto: '))
+linha = len(texto)
+escreva(texto, linha)
