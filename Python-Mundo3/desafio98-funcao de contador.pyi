6092397805ae7b0e5efd108bec78ea18fad7ddf2
ge39 <ge39.medeiros@gmail.com>
2020-05-21 21:19:53 -0300

    desafio98-funcao de contador

---
+++ Python-Mundo3/desafio98-funcao de contador.pyi
@@ -0,0 +1,10 @@
+'''
+    Faça um programa que tenha uma função chamada contador()
+    Que receba 3 parametros, inicio, fim e passo e realize a contagem
+    Seu programa tem que fazer 3 contagens, através da função criada
+
+    A) de 1 a 10, de 1 em 1
+    B) de 10 a 10, de 2 em 2
+    C) Uma contagem personalizada
+
+'''
