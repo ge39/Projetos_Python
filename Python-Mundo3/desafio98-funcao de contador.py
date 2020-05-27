d9acd47bdafcb57db972885cfe6b6976963677af
ge39 <ge39.medeiros@gmail.com>
2020-05-27 08:33:33 -0300

    desafio98-funcao de contador

--- Python-Mundo3/desafio98-funcao de contador.py
+++ Python-Mundo3/desafio98-funcao de contador.py
@@ -4,7 +4,19 @@
     Seu programa tem que fazer 3 contagens, através da função criada
 
     A) de 1 a 10, de 1 em 1
-    B) de 10 a 10, de 2 em 2
+    B) de 10 a -10, de 2 em 2
     C) Uma contagem personalizada
 
 '''
+
+def contador(i, f, p):
+    print(contador)
+
+i = int(input('Digite o inicio: '))
+f = int(input('Digite o fim: '))
+p = int(input('Digite o passo: '))
+
+for contador in range(i, f, p):
+    print(contador, end="")
+    print(end="")
+print(f'contagem de {i} até {f} de {p} em {p}  - FIM', end="")
