d07354931a605be15b10cb40bdc5c1994fe4b72b
ge39 <ge39.medeiros@gmail.com>
2020-06-02 08:31:00 -0300

    desafio99-funcao que descobre o maior

--- Python-Mundo3/desafio99-funcao que descobre o maior.py
+++ Python-Mundo3/desafio99-funcao que descobre o maior.py
@@ -7,11 +7,11 @@
 from time import sleep
 def maior(*num):
     cont = maior = 0
-    print('\nAnalisando os valores....')
+    print('\033[31m\nAnalisando os valores\033[m....')
     for valor in num:
         sleep(0.5)
         print(valor, end=" ", flush=True)
-        if cont == 0:
+        if cont == 0:  # se for o inicio da contagem
             maior = valor
         else:
             if valor > maior:
@@ -22,14 +22,10 @@ def maior(*num):
 
 
 
-
-
-
-
 #  Programa principal
 maior(2, 9, 4, 5, 7, 1)
-#maior(4, 7, 0)
-#maior(1, 2)
-#maior(6)
-#maior()
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
 
