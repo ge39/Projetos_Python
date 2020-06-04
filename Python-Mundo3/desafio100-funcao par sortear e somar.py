b2856f980ece1a99d7fc77dd7fb24f659d4d5ed2
Jeremias <ge39.medeiros@gmail.com>
2020-06-04 18:46:08 -0300

    desafio99-funcao que descobre o maior e o menor

---
+++ Python-Mundo3/desafio100-funcao par sortear e somar.py
@@ -0,0 +1,5 @@
+'''
+    faça um programa que tenha uma lista chamada numeros e duas funçoes chamaddas sorteia() e somapar().
+    A primeira função vai sortear 5 numeros, e vai coloca-los dentro da lista.
+    A segunda função vai mostrar a soma entre todos os valores Pares sorteados pela função anterior.
+'''
