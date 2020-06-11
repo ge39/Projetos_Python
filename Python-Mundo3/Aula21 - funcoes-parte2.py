387579fe99553cc9f50c4dc1224b1761da3ac093
ge39 <ge39.medeiros@gmail.com>
2020-06-11 09:09:49 -0300

    Aula21 - funcoes-parte2

--- Python-Mundo3/Aula21 - funcoes-parte2.py
+++ Python-Mundo3/Aula21 - funcoes-parte2.py
@@ -2,4 +2,18 @@
     Interactive help
     help() - função interna do python, um metodo
 '''
+def ln():
+    print('*' * 100)
+
+
+help(print)
+ln()
+help(len)
+ln()
+help(input)
+ln()
+help(slice)
+ln()
+print('IMPRIME TODO A INFORMAÇÃO DE UMA FUNÇÃO')
+print(print.__doc__)
 
