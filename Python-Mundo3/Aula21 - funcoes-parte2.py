2496cb79fb6434e13aa08627526e52e9de411b59
ge39 <ge39.medeiros@gmail.com>
2020-06-19 09:16:26 -0300

    Aula21 - funcoes-parte2

--- Python-Mundo3/Aula21 - funcoes-parte2.py
+++ Python-Mundo3/Aula21 - funcoes-parte2.py
@@ -105,8 +105,8 @@ anula a variavel global dentro do menu principal
 def teste(b):
     global a
     a = 8
-    b+=4
-    c=2
+    b += 4
+    c = 2
     print(f'A dentro vale {a}')
     print(f'B dentro vale {b}')
     print(f'C dentro vale {c}')
