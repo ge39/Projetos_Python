0a23508bb733c5a8374f94e27c28342c19b20aea
ge39 <ge39.medeiros@gmail.com>
2020-06-22 08:35:31 -0300

    desafio105-analisando e gerando dicionarios

---
+++ Python-Mundo3/desafio105-analisando e gerando dicionarios.py
@@ -0,0 +1,11 @@
+'''
+    Faça um programa que tenha uma função notas() que pode receber várias notas de alunos
+     e vai retornar um dicionario com as seguintes informações:
+
+     quantidades de notas
+     A maior nota
+     a menor nota
+     A média da turma
+     A situação(opcional)
+     Adicone tambem a docstring da função
+'''
