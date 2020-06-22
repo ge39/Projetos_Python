ef9a971b5d1013aad7c4445c7d2ebe3639a69ceb
ge39 <ge39.medeiros@gmail.com>
2020-06-22 08:39:30 -0300

    desafio106-sistema interativo de ajuda

--- Python-Mundo3/desafio106-sistema interativo de ajuda.py
+++ Python-Mundo3/desafio106-sistema interativo de ajuda.py
@@ -1,3 +1,6 @@
 '''
-
+    Faça um mini sistema, que utilize o interactive help do Python.
+    O usuario vai digitar o comando eo manual vai aparecer.
+    Quando o usuario digitar fim, o prgrama se encerrará.
+    OBS: use cores.
 '''
