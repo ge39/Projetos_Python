64afd8be293de1c7664901a4b95f6bc7120a3749
ge39 <ge39.medeiros@gmail.com>
2020-06-22 08:31:35 -0300

    desafio104-validando entradas de dados

--- Python-Mundo3/desafio104-validando entradas de dados.py
+++ Python-Mundo3/desafio104-validando entradas de dados.py
@@ -1,3 +1,7 @@
 ''''
+    Crie um programa que tenha a função leiaInt(),que vai funcionar de forma semelhante input() do python.
+    só que fazendo validações para aceitar apenas um valor numerico.
+    EX:
+    n = leiant('digite um numero')
 
 '''
