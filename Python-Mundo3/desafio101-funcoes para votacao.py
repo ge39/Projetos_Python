aeefe74908442f1bc2f4c4b2585d41b716eca661
ge39 <ge39.medeiros@gmail.com>
2020-06-22 08:20:13 -0300

    desafio101-funcoes para votacao.py-

---
+++ Python-Mundo3/desafio101-funcoes para votacao.py
@@ -0,0 +1,6 @@
+'''
+    Crie um programa que tenha uma função chamada vota() que vai receber como parametro:
+    o ano de nascimento de uma pessoa.
+    retornando um valor literal, indicando se uma pessoa tem voto NEGADO, OPCIONAL ou ÓBRIGATÓRIO nas eleiçoes.
+
+'''
