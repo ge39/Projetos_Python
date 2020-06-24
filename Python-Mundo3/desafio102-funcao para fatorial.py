413638aae8a7f57557cde83c24dcc9f125f53fef
ge39 <ge39.medeiros@gmail.com>
2020-06-24 10:09:33 -0300

    desafio102-funcao para fatorial

--- Python-Mundo3/desafio102-funcao para fatorial.py
+++ Python-Mundo3/desafio102-funcao para fatorial.py
@@ -4,3 +4,20 @@
     que será mostrado um valor LÓGICO   (opcional) indicando se será mostrado
     ou não na tela o processo de cálculo do fatorial
 '''
+
+def fatorial(n , show=False):
+    """
+    -> Calcula o Fatorial de um Numero
+    :param nr: O numero a ser calculado
+    :param show: (opcional) Mostrar ou não a conta
+    :return: O valor do Fatorial de um Numero n
+
+    """
+
+    f = 1
+    for c in range(n, 0, -1):
+       f*=c
+
+    return f
+#Programa Principal
+print(fatorial(5))
