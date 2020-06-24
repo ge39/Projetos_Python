7269bce908b99072dca76904b5f9bc5e7e425564
ge39 <ge39.medeiros@gmail.com>
2020-06-24 09:47:26 -0300

    desafio101-funcoes para votacao

--- Python-Mundo3/desafio101-funcoes para votacao.py
+++ Python-Mundo3/desafio101-funcoes para votacao.py
@@ -4,16 +4,21 @@
     retornando um valor literal, indicando se uma pessoa tem voto NEGADO, OPCIONAL ou ÓBRIGATÓRIO nas eleiçoes.
 
 '''
+
+
+
 def voto(ano):
+    from datetime import date
+    anoatual = date.today().year
+    idade = anoatual - ano
 
-    anoatual = 2020
-    if (anoatual - idade < 16):
-        print(f'voce tem {anoatual-idade} anos, Não atingiu idade para votar')
-    elif(anoatual - idade > 65):
-        print(f'Voce tem {anoatual-idade} anos, Voto opcional')
+    if (idade < 16):
+          return f'voce tem {idade} anos, Não atingiu idade para votar'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Voce tem {idade} anos, Voto opcional'
     else:
-        print(f'Voce tem {anoatual - idade} anos, Voto Obrigatório')
+        return f'Voce tem {idade} anos, Voto Obrigatório'
 
-
-idade= int(input('Em que ano voce nasceu: '))
-votar(idade)
+#Programa principal
+nasc = int(input('Em que ano voce nasceu: '))
+print(voto(nasc))
