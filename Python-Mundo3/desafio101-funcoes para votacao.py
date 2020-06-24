8ba6f40cdf5c8f510d68deb9354c5f0991272d38
ge39 <ge39.medeiros@gmail.com>
2020-06-24 09:22:15 -0300

    desafio101-funcoes para votacao

--- Python-Mundo3/desafio101-funcoes para votacao.py
+++ Python-Mundo3/desafio101-funcoes para votacao.py
@@ -1,6 +1,19 @@
 '''
-    Crie um programa que tenha uma função chamada vota() que vai receber como parametro:
+    Crie um programa que tenha uma função chamada votar() que vai receber como parametro:
     o ano de nascimento de uma pessoa.
     retornando um valor literal, indicando se uma pessoa tem voto NEGADO, OPCIONAL ou ÓBRIGATÓRIO nas eleiçoes.
 
 '''
+def votar(ano):
+    #print(idade)
+    anoatual = 2020
+    if (anoatual - idade < 16):
+        print(f'voce tem {anoatual-idade} anos, Não atingiu idade para votar')
+    elif(anoatual - idade > 65):
+        print(f'Voce tem {anoatual-idade} anos, Voto opcional')
+    else:
+        print(f'Voce tem {anoatual - idade} anos, Voto Obrigatório')
+
+
+idade= int(input('Em que ano voce nasceu: '))
+votar(idade)
