216d873c78efd79d5bb99e6f87bc3b55d89dce69
ge39 <ge39.medeiros@gmail.com>
2020-07-08 09:20:33 -0300

    desafio104-validando entradas de dados

--- Python-Mundo3/desafio104-validando entradas de dados.py
+++ Python-Mundo3/desafio104-validando entradas de dados.py
@@ -5,3 +5,23 @@
     n = leiant('digite um numero')
 
 '''
+def leiaInt(msg):
+    ok = False  #variavel recebe o valor false
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric(): #se o numero digitado for um numero
+            valor = int(n)  #a variavel valor recebe o valor de n
+            ok = True       #variavel "N" recebe o valor true
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:  # se a variavel "OK" mudou seu valor para True, finalize
+            print('ok, ',end='')
+            break
+    return valor
+
+
+#programa principal
+n = leiaInt('Digite um numero: ')
+print(f'Voce imprimiu o numero {n}')
+
