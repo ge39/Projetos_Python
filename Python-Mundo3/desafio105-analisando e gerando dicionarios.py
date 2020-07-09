5b8b44bdc5b691f390238da0059d39cedb1baa55
ge39 <ge39.medeiros@gmail.com>
2020-07-09 09:14:37 -0300

    desafio105-analisando e gerando dicionarios

--- Python-Mundo3/desafio105-analisando e gerando dicionarios.py
+++ Python-Mundo3/desafio105-analisando e gerando dicionarios.py
@@ -10,16 +10,31 @@
      Adicione tambem a docstring da função
 '''
 def notas(*n,sit=False):
+    """
+    'doscstring'
+    -> Função para analisar notas e situações de vários alunos.()
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre  situação da turma
+    """
     r = dict() #criando um dicionario para receber todas as notas
     r['total'] = len(n)
     r['maior'] = max(n)
     r['menor'] = min(n)
-    r['média'] = sum(n)/len(n)
+    r['media'] = sum(n)/len(n)
 
+    if sit: #se a situação for verdadeira "sit = True"....
+        if r['media'] >= 7:
+            r['situacao'] = 'BOA'
+        elif r['media'] >= 5:
+            r['situacao'] = 'RAZOAVEL'
+        else:
+            r['situacao'] = 'RUIM'
     return r
 
 
 
 #Programa principal
-resp = notas(2.5, 4.5, 2.5, 10, 6.5)
+resp = notas(2.5, 4.5, 2.5, 2.5, 6.5, sit = True)
 print(resp)
+help(notas)
