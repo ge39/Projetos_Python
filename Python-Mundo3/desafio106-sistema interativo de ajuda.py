8fa5312fe109e863fcf5bda434e887dbda966e5c
ge39 <ge39.medeiros@gmail.com>
2020-07-10 08:37:22 -0300

    desafio106-sistema interativo de ajuda

--- Python-Mundo3/desafio106-sistema interativo de ajuda.py
+++ Python-Mundo3/desafio106-sistema interativo de ajuda.py
@@ -4,3 +4,24 @@
     Quando o usuario digitar fim, o prgrama se encerrará.
     OBS: use cores.
 '''
+
+def ajuda(com):
+    help(com)
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print('*' * tam)
+    print(msg)
+    print('*' * tam)
+
+#programa principal
+comando=''
+while True:
+    titulo('  SISTEMA DE AJUDA PyHELP')
+    comando = str(input("Função ou Biblioteca >"))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+titulo('ATÉ LOGO')
