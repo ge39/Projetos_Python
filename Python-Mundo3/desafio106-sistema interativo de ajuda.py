b1826d3469896533083bcb1fc88751900c9d8c2c
ge39 <ge39.medeiros@gmail.com>
2020-07-10 09:14:05 -0300

    desafio106-sistema interativo de ajuda

--- Python-Mundo3/desafio106-sistema interativo de ajuda.py
+++ Python-Mundo3/desafio106-sistema interativo de ajuda.py
@@ -4,16 +4,16 @@
     Quando o usuario digitar fim, o prgrama se encerrará.
     OBS: use cores.
 '''
-#criar variavel global de cores
-c = ('\033[m',           # 0 = sem cor
-     '\033[0;30;41m\033[m',    # 1 - vermelho
-     '\033[0;30;42m',    # 2 - verde
-     '\033[0;30;43m',   # 3 - amarelo
-     '\033[0;30;44m',   # 4 - azul
-     '\033[0;30;45m',   # 5 - roxo
-     '\033[7;30m'       # 6 - branco
-    # background 40-branco;41-vermelho;43-amarelo;44-azul; 45-magenta; 46-ciano; 47-cinza
-     )
+# criar variavel global de cores
+c = ('\033[m',  # 0 = sem cor
+     '\033[0;30;41m',  # 1 - vermelho
+     '\033[0;30;42m',  # 2 - verde
+     '\033[0;30;43m',  # 3 - amarelo
+     '\033[0;30;44m',  # 4 - azul
+     '\033[0;30;45m',  # 5 - roxo
+     '\033[0;30;47m',   # 6 - cinza
+     '\033[7;30m'  # 6 - branco  # background 40-branco;41-vermelho;43-amarelo;44-azul; 45-roxo; 46-ciano; 47-cinza
+     );
 
 
 def ajuda(com):
@@ -22,14 +22,17 @@ def ajuda(com):
 
 def titulo(msg, cor=0):
     tam = len(msg) + 4
+    print(c[cor], end='')
     print('*' * tam)
     print(f'  {msg}')
     print('*' * tam)
+    print(c[0],end='')
 
-#programa principal
-comando =''
+
+# programa principal
+comando = ''
 while True:
-    titulo('SISTEMA DE AJUDA PyHELP', 1)
+    titulo('SISTEMA DE AJUDA PyHELP', 6)
     comando = str(input("Função ou Biblioteca >"))
     if comando.upper() == 'FIM':
         break
