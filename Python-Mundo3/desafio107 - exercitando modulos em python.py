57c9180976fef86fb6472cf90e1d02e4f17218d6
ge39 <ge39.medeiros@gmail.com>
2020-07-13 09:12:07 -0300

    desafio107 - exercitando modulos em python

---
+++ Python-Mundo3/desafio107 - exercitando modulos em python.py
@@ -0,0 +1,7 @@
+'''
+    crie um modulo chamado moeda.py
+    que tenha as funçoes incorporadas:
+    aumentar(), diminuir(), dobro() e metade()
+    Faça tambem um programa que importe esse módulo e use algumas dessas funções
+    
+'''
