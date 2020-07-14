a223580c42c32dd198e9ef44c570c3f0e1fbdafe
ge39 <ge39.medeiros@gmail.com>
2020-07-14 08:43:28 -0300

    desafio112-entrada de dados monetários

---
+++ Python-Mundo3/desafio112-entrada de dados monetários.py
@@ -0,0 +1,6 @@
+'''
+    Dentro do pacote utilidadeCev que criamos no desafio 111,
+    temos u, módulo chamado dado. Crie uma função leiaDinheiro()
+    que seja caoaz de funcionar como a função input().
+    mas com uma validação de dados para aceitar apenas valores que sejam monetários.
+'''
