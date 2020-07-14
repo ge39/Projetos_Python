a565f40c420cf7bd8437f899a49130842e934352
ge39 <ge39.medeiros@gmail.com>
2020-07-14 08:39:51 -0300

    desafio111- transformando modulos em pacotes

---
+++ Python-Mundo3/desafio111- transformando modulos em pacotes.py
@@ -0,0 +1,8 @@
+'''
+    Crie um pacote chamado utilidadesCev wue tenha dois modulos internos
+    chamado moeda e dado.
+
+    Transfira todos as funções utilizadas nos desafios 107, 108 e 109 para
+    o primeiro pacote e mantenha tudo funcionando.
+
+'''
