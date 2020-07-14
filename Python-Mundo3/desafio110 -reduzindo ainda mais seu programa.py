57c2e76ebd41c637a5947fc10629e04805424f58
ge39 <ge39.medeiros@gmail.com>
2020-07-14 08:56:48 -0300

    desafio110 -reduzindo ainda mais seu programa

--- Python-Mundo3/desafio110 -reduzindo ainda mais seu programa.py
+++ Python-Mundo3/desafio110 -reduzindo ainda mais seu programa.py
@@ -1,5 +1,5 @@
 '''
-    Adicione ao modulo moeda() criado no desafio anterior
+    Adicione ao mod110 moeda() criado no desafio anterior
     uma função chamada resumo(), que mostre na tela algumas
     informações geradas pela funções que já temos no módulo criado até aqui.
 '''
