5802224d336f2f52c27f2dd3ada8643395b8b999
ge39 <ge39.medeiros@gmail.com>
2020-07-14 08:29:42 -0300

    Aula22 - modulos e pacotes

--- Python-Mundo3/Aula22 - modulos e pacotes.py
+++ Python-Mundo3/Aula22 - modulos e pacotes.py
@@ -30,7 +30,7 @@ num = int(input("Digite um valor: "))
 fat = fatorial(num)
 print(f"O Fatorial de {num} é {fat}")
 ln()
-print('PACOTES - pastas que contem varios modulos')
+print('PACOTES - é uma pastas que contem varios modulos')
 '''
     dentro do pacote pode selecionar um unico modulo com o comando from pacote import modulo
 '''
