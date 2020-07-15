1e0cffc4f0753ae5e7d014b5c4e94b7abac38859
ge39 <ge39.medeiros@gmail.com>
2020-07-15 08:45:42 -0300

    moeda2

---
+++ exe108/moeda2.py
@@ -0,0 +1,16 @@
+def aumentar(n, valor):
+    s = n * 0.10 + n
+    return (s)
+
+def diminuir(n):
+    s = n * 0.10 - n
+    return(s)
+
+def dobro(n):
+   s = n * 2
+   return(s)
+
+def metade(n):
+    s = n / 2
+    return(s)
+
