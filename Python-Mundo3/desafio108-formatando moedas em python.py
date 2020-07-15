c150f59d8a0659a70d7b35b04484c9297030362e
ge39 <ge39.medeiros@gmail.com>
2020-07-15 08:55:44 -0300

    desafio108-formatando moedas em python

--- Python-Mundo3/desafio108-formatando moedas em python.py
+++ Python-Mundo3/desafio108-formatando moedas em python.py
@@ -4,3 +4,11 @@
     monetario formatado
 
 '''
+from exe108 import moeda2
+
+
+numero = float(input('Digite um Valor: R$ '))
+print(f'R$ {numero:.2f} aumentar  10 % =  R$ {moeda2.aumentar(numero, 10):.2f}')
+print(f'R$ {numero:.2f} diminuiu 10 % =  R$ {moeda2.diminuir( numero, 10):.2f}')
+print(f'O dobro de  R$ {numero} é  R$ {moeda2.dobro(numero):.2f}')
+print(f'A metade de R$ {numero} é R$ {moeda2.metade(numero):.2f}')
