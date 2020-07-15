bcaf553e9ca5c5877110c0303f4515fd0956c44f
ge39 <ge39.medeiros@gmail.com>
2020-07-15 08:42:44 -0300

    desafio107 - exercitando modulos em python

--- Python-Mundo3/desafio107 - exercitando modulos em python.py
+++ Python-Mundo3/desafio107 - exercitando modulos em python.py
@@ -5,9 +5,9 @@
     Faça tambem um programa que importe esse módulo e use algumas dessas funções
     
 '''
-import moeda
+from exe107 import moeda
 
-numero = float(input('Digite um Valor: '))
+numero = float(input('Digite um Valor: R$ '))
 
 print(f'R$ {numero:.2f} aumentar  10 % =  R$ {moeda.aumentar(numero):.2f}')
 print(f'R$ {numero:.2f} diminuiu 10 % =  R$ {moeda.diminuir( - numero):.2f}')
