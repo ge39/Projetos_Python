e57c81eff59b73e326084f467c4ca1b458604f4a
ge39 <ge39.medeiros@gmail.com>
2020-07-15 08:25:58 -0300

    moeda

--- Python-Mundo3/moeda.py
+++ Python-Mundo3/moeda.py
@@ -3,14 +3,14 @@ def aumentar(n):
     return (s)
 
 def diminuir(n):
-    n - 1
-    return(numero)
+    s = n * 0.10 - n
+    return(s)
 
-def dobro():
-    n * 2
-    return(numero)
+def dobro(n):
+   s = n * 2
+   return(s)
 
-def metade():
-    n / 2
-    return(numero)
+def metade(n):
+    s = n / 2
+    return(s)
 
