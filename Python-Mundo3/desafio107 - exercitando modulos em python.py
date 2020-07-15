58f8c77b58229a1f99cf0e5d03dcae673f926e9f
ge39 <ge39.medeiros@gmail.com>
2020-07-15 08:25:44 -0300

    desafio107 - exercitando modulos em python

--- Python-Mundo3/desafio107 - exercitando modulos em python.py
+++ Python-Mundo3/desafio107 - exercitando modulos em python.py
@@ -5,23 +5,11 @@
     Faça tambem um programa que importe esse módulo e use algumas dessas funções
     
 '''
-def aumentar(n):
-    s = n * 0.10 + n
-    return (s)
-
-def diminuir(n):
-    n - 1
-    return(numero)
-
-def dobro():
-    n * 2
-    return(numero)
-
-def metade():
-    n / 2
-    return(numero)
-
+import moeda
 
 numero = float(input('Digite um Valor: '))
 
-print(f'10 % de R$ {numero} é R$ {aumentar(numero)}')
+print(f'R$ {numero:.2f} aumentar  10 % =  R$ {moeda.aumentar(numero):.2f}')
+print(f'R$ {numero:.2f} diminuiu 10 % =  R$ {moeda.diminuir( - numero):.2f}')
+print(f'O dobro de  R$ {numero} é  R$ {moeda.dobro(numero):.2f}')
+print(f'A metade de R$ {numero} é R$ {moeda.metade(numero):.2f}')
