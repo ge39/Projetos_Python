b642003dbf2577dfdcde70637fb4c488965d00d1
ge39 <ge39.medeiros@gmail.com>
2020-07-17 07:29:20 -0300

    desafio110 -reduzindo ainda mais seu programa

--- Python-Mundo3/desafio110 -reduzindo ainda mais seu programa.py
+++ Python-Mundo3/desafio110 -reduzindo ainda mais seu programa.py
@@ -3,3 +3,6 @@
     uma função chamada resumo(), que mostre na tela algumas
     informações geradas pela funções que já temos no módulo criado até aqui.
 '''
+from exe110 import moeda
+
+numero = float(input("Digite um valor: R$ "))
