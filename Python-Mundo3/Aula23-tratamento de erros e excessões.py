96185bac4895d99b1033c7c3c0ea309c536f7ffd
ge39 <ge39.medeiros@gmail.com>
2020-07-18 08:39:08 -0300

    Aula23-tratamento de erros e excessões

--- Python-Mundo3/Aula23-tratamento de erros e excessões.py
+++ Python-Mundo3/Aula23-tratamento de erros e excessões.py
@@ -5,4 +5,28 @@
 def ln():
     print('*' * 50)
 
+#programa principal
 ln()
+'''
+try
+    operacao
+Exception
+    error
+'''
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador :'))
+    r = a / b
+#except Exception as erro:
+except (ValueError, TypeError):
+    print(f'Tivemos um problema com os dados que voce digitou :')
+except ZeroDivisionError:
+    print('Não é possivel dividir um numero por zero')
+except KeyboardInterrupt:
+    print('O usuario preferiu nao informar os dados')
+except Exception as erro:
+    print(f'O encontrado foi {erro.__cause__}')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte Sempre! muito obrigado.')
