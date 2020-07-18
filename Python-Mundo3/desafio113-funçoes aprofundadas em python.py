9b2445fdcad95fba20968cf7b8e86539b5fc1f69
ge39 <ge39.medeiros@gmail.com>
2020-07-18 08:46:50 -0300

    desafio113-funçoes aprofundadas em python

---
+++ Python-Mundo3/desafio113-funçoes aprofundadas em python.py
@@ -0,0 +1,6 @@
+'''
+    Reescreva a função leiaInt()
+    que fizemos no desafio104, incluindo agora a possibilidade da digitação
+    de um numero de tipo inválido.
+    aproveite e crie uma função leiaFloat(), com a mesma funcionalidade
+'''
