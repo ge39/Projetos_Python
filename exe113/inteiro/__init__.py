fca30cac1823c17dc0227b1ca623cff3834eb92e
ge39 <ge39.medeiros@gmail.com>
2020-07-21 20:39:27 -0300

    init

--- exe113/inteiro/__init__.py
+++ exe113/inteiro/__init__.py
@@ -1,7 +1,7 @@
 def leiaInt(msg):
     valido = False
     while not valido:
-        entrada = (input(msg))
+        entrada = int(input(msg))
 
         if entrada.isfloat():
             print(f'\033[31mValor {entrada} incorreto, Digite um valor do Tipo inteiro\033[m')
