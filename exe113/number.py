6e91b027ee9051118f774790899ae50614f76e65
ge39 <ge39.medeiros@gmail.com>
2020-07-21 20:08:03 -0300

    number

--- exe113/number.py
+++ exe113/number.py
@@ -1,10 +1,12 @@
 def leiaInt(msg):
     valido = False
-
     while not valido:
         entrada = int(input(msg)).strip()
         if entrada.isalpha() or entrada.isfloat():
-            print(f'\033[31mValor {entrada} incorreto, Digite um valor do Tipo inteiro\033[m')
+            print(f'\033[31mValor {entrada} incorreto, Digite um valor do Tipo inteiro\033[m'
+        else:
+            valido = true
+            return int(entrada)
 
 
 
