1d65daf530ac94823582e942549262c04d781ed5
ge39 <ge39.medeiros@gmail.com>
2020-07-21 21:27:13 -0300

    desafio114-site está acessivel

--- Python-Mundo3/desafio114-site está acessivel.py
+++ Python-Mundo3/desafio114-site está acessivel.py
@@ -2,3 +2,15 @@
     crie um código em Python que teste se o site Pudim está
     acessivel pelo computador do usado.
 '''
+
+import urllib
+import urllib.request
+
+try:
+    link = 'http://foodsti.epizy.com'
+    site = urllib.request.urlopen('http://foodsti.epizy.com')
+except urllib.error.URLError:
+    print(F'O site {link} não está acessivel no momento')
+else:
+    print(f'Site {link} está acessivel')
+    print(site.read())
