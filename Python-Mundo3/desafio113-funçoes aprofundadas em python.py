0331c11ae83cfad069549735a3cd76366c596a33
ge39 <ge39.medeiros@gmail.com>
2020-07-21 21:13:14 -0300

    desafio113-funçoes aprofundadas em python

--- Python-Mundo3/desafio113-funçoes aprofundadas em python.py
+++ Python-Mundo3/desafio113-funçoes aprofundadas em python.py
@@ -4,13 +4,9 @@
     de um numero de tipo inválido.
     aproveite e crie uma função leiaFloat(), com a mesma funcionalidade
 '''
+from exe113 import numeros
+#programa principal
+n1 = numeros.leiaInt("Digite um numero: ")
+n2 = numeros.leiaFloat("Digite um valor Real: ")
+print(f'O valor inteiro foi \033[33m{n1}\033[m e o valor real foi \033[34m{n2:.2f}\033[m')
 
-def leiaInt():
-    while True:
-        try:
-            n = int(input("Digite um numero: "))
-        except(ValueError, TypeError):
-            print('ERROR!!, Digite um numero inteiro valido')
-            continue
-        else:
-            return n
