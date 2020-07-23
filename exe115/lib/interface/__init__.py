1eb8dcffce5e64f1c69ad8974a619d60f19185c9
ge39 <ge39.medeiros@gmail.com>
2020-07-23 18:48:02 -0300

    interface

--- exe115/lib/interface/__init__.py
+++ exe115/lib/interface/__init__.py
@@ -1,8 +1,16 @@
 def linha(tam = 42):
-    return '-' * tam
+    return '\033[35m-\033[m' * tam
 
 
 def cabecalho(txt):
     print(linha())
     print(txt.center(42))
+    print(linha())
+
+def menu(lista):
+    cabecalho('\033[34mMENU PRINCIPAL\033[m')
+    c = 1
+    for item in lista:
+        print(f'{c} - {item}')
+        c += 1
     print(linha())
