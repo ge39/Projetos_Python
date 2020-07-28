41129eb2acd4eb18ab7c7f27297388b292915909
ge39 <ge39.medeiros@gmail.com>
2020-07-28 20:11:07 -0300

    sistema

--- exe115/sistema.py
+++ exe115/sistema.py
@@ -1,16 +1,17 @@
 from exe115.lib.interface import *
+from time import sleep
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar noca pessoas', 'Sair do Sistema'])
-
+    print('processando.....')
+    sleep(1.5)
     if resposta == 1:
-        print(f'opção 1')
-
+        cabecalho(f'opção 1')
     elif resposta == 2:
-        print(f'Opcao 2')
+        cabecalho(f'Opcao 2')
     elif resposta == 3:
-        print(f'Opcao 3')
-        print(f'Saindo do Sistema, Até Logo')
+        cabecalho(f'Opcao 3')
+        cabecalho(f'\033[0;30;46mSaindo do Sistema, Até Logo\033[m')
     else:
-        print('\033[31mERROR!! Digite uma opção válida')
-
+        cabecalho('\033[31mERROR!! Digite uma opção válida')
+        sleep(2)
