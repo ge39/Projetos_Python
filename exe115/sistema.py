476552cc8063dd50e763fba79bd95953045d097b
ge39 <ge39.medeiros@gmail.com>
2020-07-28 19:54:02 -0300

    sistema

--- exe115/sistema.py
+++ exe115/sistema.py
@@ -1,4 +1,16 @@
 from exe115.lib.interface import *
 
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar noca pessoas', 'Sair do Sistema'])
 
-resposta = menu(['Ver pessoas cadastradas', 'Cadastrar noca pessoas', 'Sair do Sistema'])
+    if resposta == 1:
+        print(f'opção 1')
+
+    elif resposta == 2:
+        print(f'Opcao 2')
+    elif resposta == 3:
+        print(f'Opcao 3')
+        print(f'Saindo do Sistema, Até Logo')
+    else:
+        print('\033[31mERROR!! Digite uma opção válida')
+
