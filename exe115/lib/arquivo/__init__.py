0eba47d95ecdcb205a16ced10ca23a632902a81e
ge39 <ge39.medeiros@gmail.com>
2020-07-28 20:56:29 -0300

    arquivo

--- exe115/lib/arquivo/__init__.py
+++ exe115/lib/arquivo/__init__.py
@@ -1,3 +1,6 @@
+from exe115.lib.interface import*
+
+
 def arquivoExiste(nome):
     try:
         a = open(nome, 'rt')
@@ -7,6 +10,7 @@ def arquivoExiste(nome):
     else:
         return True
 
+
 def criarArquivo(nome):
     try:
         a = open(nome, 'wt+') # cria um arquivo novo
@@ -15,3 +19,12 @@ def criarArquivo(nome):
         print('Houve um erro na criação do arquivo!')
     else:
         print(f'Arquivo \033[32m{nome}\033[m criado com sucesso!')
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        print(a.readlines())
