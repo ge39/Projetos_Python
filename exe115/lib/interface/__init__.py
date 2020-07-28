fb5271e176dae0b98bb398000bbaa8427a70908e
ge39 <ge39.medeiros@gmail.com>
2020-07-28 19:05:09 -0300

    interface

--- exe115/lib/interface/__init__.py
+++ exe115/lib/interface/__init__.py
@@ -1,3 +1,17 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except(ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um numero inteiro válido.\033[m')
+            continue
+        except(KeyboardInterrupt):
+            print('\n\033[m31Usuaário preferiu não digitar esse numero.\033[m')
+            return 0
+        else:
+            return n
+
+
 def linha(tam = 42):
     return '\033[35m-\033[m' * tam
 
@@ -7,10 +21,13 @@ def cabecalho(txt):
     print(txt.center(42))
     print(linha())
 
+
 def menu(lista):
     cabecalho('\033[34mMENU PRINCIPAL\033[m')
     c = 1
     for item in lista:
-        print(f'{c} - {item}')
+        print(f'\033[31m{c}\033[m - {item}')
         c += 1
     print(linha())
+    opc = leiaInt('\033[34mSua opção:\033[m ')
+    return opc
