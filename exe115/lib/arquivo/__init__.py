ec9d2fad41a6d96720c4c07baa446eaf29cea920
ge39 <ge39.medeiros@gmail.com>
2020-07-28 20:45:11 -0300

    arquivo

--- exe115/lib/arquivo/__init__.py
+++ exe115/lib/arquivo/__init__.py
@@ -6,3 +6,12 @@ def arquivoExiste(nome):
         return False
     else:
         return True
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+') # cria um arquivo novo
+        a.close()
+    except:
+        print('Houve um erro na criação do arquivo!')
+    else:
+        print(f'Arquivo \033[32m{nome}\033[m criado com sucesso!')
